188d566aefe93d172c06da72e881462bc37e09c6
Zach S.L <zach.s.ledford@gmail.com>
2020-08-10 23:53:33 -0400

    showing sorted live cases on the table

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -8,12 +8,15 @@ import {
 } from "@material-ui/core";
 import InfoBox from "./components/InfoBox";
 import Map from "./components/Map";
+import Table from "./components/Table";
+import { sortData } from "./util";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState([]);
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -32,6 +35,8 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -102,7 +107,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live cases by Country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwide new cases</h3>
           {/* Graph */}
         </CardContent>
