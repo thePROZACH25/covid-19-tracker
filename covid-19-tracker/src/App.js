dc571c6d19776a16de66cf1e8b615f301f15cbfc
Zach S.L <zach.s.ledford@gmail.com>
2020-08-12 05:26:14 -0400

    showing cases with circles on the map

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -20,7 +20,8 @@ function App() {
   const [countryInfo, setCountryInfo] = useState([]);
   const [tableData, setTableData] = useState([]);
   const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
-  const [mapZoom, setmapZoom] = useState(3);
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -41,6 +42,7 @@ function App() {
           }));
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
         });
     };
@@ -58,9 +60,10 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         setCountry(countryCode);
-        // All of the data
-        // from the country response
         setCountryInfo(data);
+
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -103,7 +106,7 @@ function App() {
           />
         </div>
 
-        <Map center={mapCenter} zoom={mapZoom} />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
