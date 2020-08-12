410d0189a3b49238ec164305173f064f86275ae8
Zach S.L <zach.s.ledford@gmail.com>
2020-08-12 06:19:44 -0400

    adding an onClick on the infobox to change the graphics on the map

--- covid-19-tracker/src/components/InfoBox.js
+++ covid-19-tracker/src/components/InfoBox.js
@@ -2,16 +2,18 @@ import React from "react";
 import "../components/InfoBox.css"
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, total , ...props}) {
   return (
-    <Card className="infoBox">
+    <Card 
+    onClick={props.onClick}
+    className="infoBox">
       <CardContent>
         {/* title */}
         <Typography className='infoBox__title' color="textSecondary">{title}</Typography>
         {/* number of cases */}
         <h2 className='infoBox__cases'>{cases}</h2>
         {/* Total */}
-        <Typography className='infoBox__total'>Total - {total}</Typography>
+        <Typography className='infoBox__total'>Total: {total}</Typography>
       </CardContent>
     </Card>
   );
